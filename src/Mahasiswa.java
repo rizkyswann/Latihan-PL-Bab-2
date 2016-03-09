afa2fb7c20507136d6b0e476fad0f68246b5dddf
Rizkyswann <Rizkyswann>
2016-03-09 20:16:14 +0700

    update mainProgram.java dan Mahasiswa.java

---
+++ src/Mahasiswa.java
@@ -0,0 +1,8 @@
+import java.utill.Scanner;
+public class Mahasiswa {
+	Scanner masukan = new Scanner (System.in);
+	String nama_mahasiswa, nim;
+	double jarak, jumlahPutaran, jumlah_waktu;
+	int keliling, jumlah_mahasiswa;
+
+}
---
+++ src/mainProgram.java
@@ -0,0 +1,19 @@
+import java.utill.Scanner;
+public class mainProgram{
+	public static void main (String [] args){
+	Scanner masukan = new Scanner (System.in);
+	int jumlah_mahasiswa, p, l, max=0;
+	jumlah_mahasiswa = masukan.nextInt();
+	p = masukan.nextInt();
+	l = masukan.nextInt();
+
+	System.out.println("##INPUT##");
+	Mahasiswa mhs[] = new Mahasiswa[jumlah_mahasiswa];
+	for (int a=0; a<jumlah_mahasiswa; a++){
+		String nim = maukan.next();
+		String nama_mahasiswa = masukan.next();
+		double jumlahPutaran = masukan.nextDouble();
+		double jumlah_waktu = masukan.nextDouble();
+	}
+	}
+}
